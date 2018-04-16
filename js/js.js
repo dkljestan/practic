23028961df1a0f8e962c56d7d39c6756ef989263
David Kljestan <34009757+dkljestan@users.noreply.github.com>
2018-04-16 09:31:22 +0200

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,31 @@
+document.querySelector(".bar").addEventListener("click",function(){
+	document.querySelector(".menu").classList.toggle("isOpen");	
+});
+
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+function topFunction() {
+    $('html,body').animate({
+    	"scrollTop": 0 
+    },1000);
+}
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+});
